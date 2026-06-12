383b809b1803aaae7b0ff379406997c578e567bb
Mike Smith <mike_smith@harvard.edu>
2026-06-12 18:36:56 -0400

    Move from dev

---
+++ resilient_turn.py
@@ -0,0 +1,23 @@
+### m07b/resilient_turn.py
+import random
+
+def main():
+    secret = random.randint(1, 100)
+    print(f"The secret number is {secret}")
+
+    # Grab the player's guess
+    try:
+        guess = int(input('Please input your guess: '))
+    except ValueError:
+        print('Guesses must be an integer.')
+
+    # Check guess against the secret
+    if guess == secret:
+        print('Exactly! You win!')
+    elif guess < secret:
+        print('Too small!')
+    else:
+        print('Too big!')
+
+if __name__ == '__main__':
+    main()
